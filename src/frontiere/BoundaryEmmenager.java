aba7c646335ede9e8da5d4856056070dd8e8a370
Your Name <mathildedarques.m@gmail.com>
2023-02-23 20:01:28 +0100

    tp2

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -11,8 +11,7 @@ public class BoundaryEmmenager {
 
 	public void emmenager(String nomVisiteur) {
 		if (controlEmmenager.isHabitant(nomVisiteur)) {
-			System.out.println(
-					"Mais vous êtes déjà un habitant du village !");
+			System.out.println("Mais vous êtes déjà un habitant du village !");
 		} else {
 			StringBuilder question = new StringBuilder();
 			question.append("Êtes-vous :\n");
@@ -27,19 +26,39 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					emmenagerGaulois(nomVisiteur);
 					break;
 
 				default:
-					System.out
-							.println("Vous devez choisir le chiffre 1 ou 2 !");
+					System.out.println("Vous devez choisir le chiffre 1 ou 2 !");
 					break;
 				}
 			} while (choixUtilisateur != 1 && choixUtilisateur != 2);
 		}
 	}
 
-	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+	private void emmenagerGaulois(String nomVisiteur) {
+		int force=-1;
+		System.out.println("Bienvenu villageois "+nomVisiteur);
+		force=Clavier.entrerEntier("Quelle est votre force?");
+		controlEmmenager.ajouterGaulois(nomVisiteur, force);
 	}
+
+	private void emmenagerDruide(String nomVisiteur) {
+		int force = -1;
+		int potionMin =-1;
+		int potionMax=-1;
+		System.out.println("Bienvenue druide " + nomVisiteur);
+		force = Clavier.entrerEntier("Quelle est votre force ?");
+		potionMin = Clavier.entrerEntier("Quelle est la force de potion la plus faible que vous produisez ?");
+		do {
+			potionMax = Clavier.entrerEntier("Quelle est la force de potion la plus forte que vous produisez ?");
+			if (potionMax<potionMin) {
+				System.out.println("Votre force de potion la plus forte ne peut pas �tre plus petite que votre force de potion la plus faible");
+			}
+		} while(potionMax<potionMin);
+
+	controlEmmenager.ajouterDuide(nomVisiteur,force,potionMin,potionMax);
+	}
+	
 }
--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -1,5 +1,6 @@
 package frontiere;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import controleur.ControlPrendreEtal;
@@ -13,10 +14,45 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
-	}
+		if(!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.println("Je suis désolé "+nomVendeur+" il faut être un habitant du village pour commercer ici");
+		} else {
+			System.out.println("Bonjour "+nomVendeur+" je vais regarder si je peux vous trouvez un étal");
+			if(!controlPrendreEtal.resteEtals()) {
+				System.out.println("Désolé "+nomVendeur+" je n'ai plus d'étals qui ne soit pas déjà occupé");
+			} else {
+				installerVendeur(nomVendeur);
+			}
+		}
+		}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		System.out.println("C'est parfait, il me reste un étal pour vous !");
+		System.out.println("Il me faudrait quelques renseignements:");
+		String produit = entreString("Quel produit souhaitez-vous vendre ?");
+		int nbProduit= Clavier.entrerEntier("Combien souhaitez-vous en vendre?");
+		int numEtal= controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		if(numEtal!=-1) {
+			System.out.println("Le vendeur "+nomVendeur+" s'est installé à l'étal numéro "+numEtal);
+		}
+		}
+	
+	
+	private String entreString(String question) {
+		boolean entreecorrecte=false;
+		String produit = null;
+		do {
+			try {
+				System.out.println(question);
+				produit=scan.next();
+				entreecorrecte=true;
+			} catch (InputMismatchException e) {
+				System.out.println("Vous devez entrer du texte !");
+				entreecorrecte=false;
+			}
+		} while(!entreecorrecte);
+		return produit;
 	}
+	
+	
 }
